ee46c0c79087bf6baaf776768a7fafc5dce61cbb
Utsab <utsab.k.saha@gmail.com>
2017-11-12 05:03:21 +0000

    React app, added proxy
    * Having problem to recognizing the host

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//   res.send('respond with a resource');
+
+     res.json([{
+         id: 1,
+         username: 'tom'
+     }, {
+         id: 2,
+         username: 'utsab'
+     }])
 });
 
 module.exports = router;
