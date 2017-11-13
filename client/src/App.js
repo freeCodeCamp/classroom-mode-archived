2a1e6d5a920868e21687d2efd355c98904b31f04
Utsab <utsab.k.saha@gmail.com>
2017-11-13 05:09:00 +0000

    Add fetch from React component to express server.  Still does not work but closer than before

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {users: []};
+  
+  componentDidMount() {
+    fetch('/users')
+      .then(res => res.json())
+      .then(users => this.setState({ users }));
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Users</h1>
+        {this.state.users.map(user =>
+          <div key={user.id}>{user.username}</div>
+        )}
       </div>
     );
   }
--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
-
+     console.log('users.js')
      res.json([{
          id: 1,
          username: 'tom'
