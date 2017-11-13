66c0fd8a06d7f2b10755fea46707c567f4415dbb
Utsab <utsab.k.saha@gmail.com>
2017-11-13 05:09:00 +0000

    Add fetch from React component to express server.  Still does not work but closer than before

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
-
+console.log('server running');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log('index.js');
   res.render('index', { title: 'Express' });
 });
 
