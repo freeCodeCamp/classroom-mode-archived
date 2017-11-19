aa46c8a243a22bd9fb9f284a87d00c99daff2aee
Utsab <utsab.k.saha@gmail.com>
2017-11-19 04:59:14 +0000

    Fix path to the client/build directory

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 
 //app.use('/', index);
