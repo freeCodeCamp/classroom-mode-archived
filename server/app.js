b832d3aaaa4cc19963551583a7b59b6437603e6d
Utsab <utsab.k.saha@gmail.com>
2017-11-19 04:55:42 +0000

    Adding client routing stuff back in again

--- server/app.js
+++ server/app.js
@@ -20,12 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 
-app.use('/', index);
+//app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
