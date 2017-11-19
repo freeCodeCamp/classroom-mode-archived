f7dd2472e12d8f5ce8c59870e231491efc195672
Utsab <utsab.k.saha@gmail.com>
2017-11-19 04:49:47 +0000

    Handle static requests to the express server by serving files from the client folder, which is where the react components live

--- server/app.js
+++ server/app.js
@@ -20,9 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+//app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
