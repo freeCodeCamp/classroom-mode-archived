6617f1b9279f487f68b1b5f917a598781fff1eaf
Utsab <utsab.k.saha@gmail.com>
2017-11-20 20:50:10 +0000

    Add libraries for unit testing with mocha chai.  Add one dummy test.  Also updated the .gitignore to ignore the top-level package-log.json file

---
+++ test/scraper.js
@@ -0,0 +1,15 @@
+var should = require('chai').should(); 
+
+
+it('should list ALL blobs on /blobs GET', function(done) {
+//   chai.request(server)
+//     .get('/blobs')
+//     .end(function(err, res){
+//       res.should.have.status(200);
+//       done();
+//     });
+
+    // foo.should.be.a('string2');
+    foo.should.be.a('string');
+    done(); 
+});
