d9d62670b1663cd45626e7cd78231150f85a8794
Utsab <utsab.k.saha@gmail.com>
2017-11-20 05:24:04 +0000

    Ping Tom's scraper from the /users route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+
+
+const fccBaseUrl = 'https://fcc-profile-scraper.herokuapp.com/user/';
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/:userId', function(req, res, next) {
 //   res.send('respond with a resource');
-     console.log('users.js')
-     res.json([{
-         id: 1,
-         username: 'tom'
-     }, {
-         id: 2,
-         username: 'utsab'
-     }])
+
+     console.log("params: ");
+     console.log(req.params);
+     let githubName = req.params.userId; 
+     
+     request(fccBaseUrl + githubName, function (error, response, body){
+          console.log("Received scraper response"); 
+          
+          if (error) {
+               // throw error;
+               // reject();
+               console.log("Error: " + error); 
+          }
+         
+          body = JSON.parse(body);
+          res.json(body); 
+     }); 
 });
 
 module.exports = router;
