19fcfabd0b49357214a603a7c0ebf63ecc7a5c01
Utsab <utsab.k.saha@gmail.com>
2017-11-21 01:56:26 +0000

    Add name filter for searching for a specific github name

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 function DisplayUserInfo(props) {
      if (props.user_info.name) {
        return (
@@ -21,21 +22,27 @@ function DisplayUserInfo(props) {
 class App extends Component {
   state = {user_info: {}};
   
-  componentDidMount() {
-    fetch('/users/utsab')
+  componentDidMount() {}
+  
+  handleSearch(e) {
+    var githubName = document.getElementById('github-name-input').value;
+    
+    if (!githubName) return; 
+    
+
+    fetch('/users/' + githubName)
       .then(res => res.json())
       .then(function(user_info) {
-        debugger
         this.setState({ user_info });
       }.bind(this));
-  }
-  
-  
+  };
 
   render() {
     return (
       <div className="App">
         <h1>User Info</h1>
+          <input type="text" id="github-name-input"></input>
+          <button onClick={this.handleSearch.bind(this)}>Search</button>
           <DisplayUserInfo user_info={this.state.user_info}/>
       </div>
     );
