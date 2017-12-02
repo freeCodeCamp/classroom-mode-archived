a4c588b61e88513e497f8301efd3465576bcdea8
Tom Lee <tlee418@g.ucla.edu>
2017-12-02 05:07:36 +0000

    Introduce React Bootstrap
    * Tested with button component

--- client/src/AddStudentForm.js
+++ client/src/AddStudentForm.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class AddStudentForm extends Component {
   render() {
     return (
         <div className="AddStudentForm">
+            <Button bsSize="large" active>Button</Button>
             <form>
                 <input type="text" />
                 <input type="submit" />
