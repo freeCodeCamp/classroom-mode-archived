3ec9282b528cdedd9a649d8ce582c5d7d46dece2
Tom Lee <tlee418@g.ucla.edu>
2017-12-02 10:07:08 +0000

    Button to trigger Modal
    * Followed React Bootstrap Example
    * open & close function not working properly

--- client/src/AddStudentForm.js
+++ client/src/AddStudentForm.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
+
 
 class AddStudentForm extends Component {
+  constructor(props){
+    super(props); 
+    this.state = { showModal: false };
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+  }
+
+  close() {
+    this.setState({showModal: false});
+  }
+
+  open() {
+    this.setState({showModel: true});
+  }
+
   render() {
     return (
         <div className="AddStudentForm">
-            <Button bsSize="large" active>Button</Button>
-            <form>
-                <input type="text" />
-                <input type="submit" />
-            </form>
+            <Button bsSize="large" active onClick={this.open}>Button</Button>
+            <Modal show={this.state.showModal} onHide={this.close}>
+              <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <h4>Text in a modal</h4>
+                <form>
+                    <input type="text" />
+                    <input type="submit" />
+                </form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={this.close}>Close</Button>
+              </Modal.Footer>
+            </Modal>
         </div>
     );
   }
