dcd4efffeafd862cf0dc8835a10d76de9945797a
Tom Lee <user512@users.noreply.github.com>
2017-12-04 05:12:38 +0000

    Add Student From can now reach Exress server

---
+++ server/routes/addStudent.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var databaseModule = require('./helpers/database_singleton');
+var db = databaseModule.getDbInstance();
+
+router.post('/', function(req, res) {
+    console.log('hitting server');
+    console.log("params:" + req.body.name);
+})
+
+module.exports = router;
