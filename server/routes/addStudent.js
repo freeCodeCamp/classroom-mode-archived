03fdac8e09740406b202dc945c59bcb458a1a22e
Tom Lee <user512@users.noreply.github.com>
2017-12-05 04:41:39 +0000

    Can save student to db upon form submission

--- server/routes/addStudent.js
+++ server/routes/addStudent.js
@@ -3,10 +3,20 @@ var router = express.Router();
 var request = require('request');
 var databaseModule = require('./helpers/database_singleton');
 var db = databaseModule.getDbInstance();
+var Student = require('./helpers/studentSchema');
 
 router.post('/', function(req, res) {
-    console.log('hitting server');
-    console.log(req.body);
+    var student = new Student({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        notes: req.body.notes
+    })
+
+    student.save(function(err, fluffy) {
+        if (err) return console.error(err);
+        console.log(student);
+    })
 })
 
 module.exports = router;
