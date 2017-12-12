993946b3bc2a8393bd61512b3fd6dc7b3bb02542
Utsab <utsab.k.saha@gmail.com>
2017-12-12 04:54:24 +0000

    Add Enzyme testing framework and one valid unit test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+import { mount } from "enzyme";
 import ReactDOM from 'react-dom';
 import App from './App';
+import AddStudentForm from './AddStudentForm';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+
+describe("AddStudentForm", () => {
+  let props;
+  let mountedAddStudentForm;
+  const addStudentForm = () => {
+    if (!mountedAddStudentForm) {
+      mountedAddStudentForm = mount(
+        <AddStudentForm {...props} />
+      );
+    }
+    return mountedAddStudentForm;
+  }
+
+  beforeEach(() => {
+    props = {
+      wallpaperPath: undefined,
+      userInfoMessage: undefined,
+      onUnlocked: undefined,
+    };
+    mountedAddStudentForm = undefined;
+  });
+  
+  it("always renders a div", () => {
+    const divs = addStudentForm().find("div");
+    expect(divs.length).toBeGreaterThan(0);
+  });
 });
