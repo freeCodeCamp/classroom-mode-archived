e0c96a5489e3059c9259c72a07cd58250a883fd2
Utsab <utsab.k.saha@gmail.com>
2017-12-15 06:33:53 +0000

    Add a test for scraper helper that stubs the external response

--- server/test/scraper.js
+++ server/test/scraper.js
@@ -1,4 +1,5 @@
 var should = require('chai').should(); 
+var expect = require('chai').expect; 
 var scraper = require('../routes/helpers/scraper'); 
 var sinon = require('sinon');
 var request = require('request');
@@ -6,17 +7,23 @@ var request = require('request');
 
 
 it('should return an error if scraper has a non-200 status code', function(done) {
+   var get = sinon.stub(request, "get");
    
+   get.yieldsOn(this, null, {statusCode:400}, "{}");
    
+   scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
+       expect(err).to.equal(true); 
+       expect(results.error).to.be.a('object'); 
+       done(); 
+   })
+   get.restore(); 
 });
 
 
 
 it('should return no errors if scraper has a 200 status code', function(done) {
-     
+  
 });
-
-
 
 it('should compute correct number of inactive days', function(done) {
      
