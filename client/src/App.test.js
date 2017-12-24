d824249e44d459445bcb05157f47b51251aa4ccc
Utsab <utsab.k.saha@gmail.com>
2017-12-24 05:36:34 +0000

    Created react unit test that mocks the ajax response to return a 200 OK.  Not fully working yet.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -7,8 +7,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import AddStudentForm from './AddStudentForm';
 
-
-
 describe("AddStudentForm", () => {
   let props;
   let mountedAddStudentForm;
@@ -33,5 +31,39 @@ describe("AddStudentForm", () => {
   it("always renders a div", () => {
     const divs = addStudentForm().find("div");
     expect(divs.length).toBeGreaterThan(0);
+  });
+  
+  
+
+  it("should close the modal when submitting returns a 200 response", () => {
+    
+    const mockResponse = (status, statusText, response) => {
+      return new window.Response(response, {
+        status: status,
+        statusText: statusText,
+        headers: {
+          'Content-type': 'application/json'
+        }
+      });
+    };
+  
+    window.fetch = jest.fn().mockImplementation(function() {
+        console.log("In the mocked fetch function"); 
+        return Promise.resolve(mockResponse(200, null, '{"foo":"bar"}')); 
+      }
+    );
+      
+      
+    var addStudentFormComponentWrapper = addStudentForm(); 
+    addStudentFormComponentWrapper.instance().open(); 
+    addStudentFormComponentWrapper.instance().submit(); 
+    console.log("state: "); 
+    console.log(addStudentFormComponentWrapper.instance().state); 
+    
+    // This is not working yet because it gets executed immediately.  However, the fetch happens asyncronously, 
+    // so we won't see the impact of the fetch until later.  
+    expect(addStudentFormComponentWrapper.instance().state.showModal).toBe(false); 
+    
+  
   });
 });
