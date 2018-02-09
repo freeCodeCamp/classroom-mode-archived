e12cccec882a936e020a0e0e8da1de6b4f233b9c
Utsab <utsab.k.saha@gmail.com>
2018-02-09 05:16:35 +0000

    Stub db collection's find method

--- server/routes/showStudents.js
+++ server/routes/showStudents.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./helpers/database_singleton').getDbInstance(); 
+
 
 router.get('/', function(req, res) {
   res.sendStatus(200);
