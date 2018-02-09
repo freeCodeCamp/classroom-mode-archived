8ca202dbf9c288ccad62902730a94f97f657d542
Utsab <utsab.k.saha@gmail.com>
2018-02-09 05:16:35 +0000

    Stub db collection's find method

--- server/test/showStudentTest.js
+++ server/test/showStudentTest.js
@@ -2,6 +2,8 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 var app = require('../app');
 var request = require('supertest');
+var db = require('../routes/helpers/database_singleton').getDbInstance(); 
+
 
 describe('GET /students', () => {
   it('should return 200', (done) => {
@@ -12,7 +14,36 @@ describe('GET /students', () => {
         done();
       });
   });
+  
+  it('should fetch data from mongo DB', (done) => {
+    var dummyStudentResults = [{ _id: "5a28cd1b1805592081cd31ea",
+      name: 'asff',
+      username: 'asdf',
+      email: 'asdf',
+      notes: '',
+      __v: 0 }]; 
+      
+    sinon.stub(db, "collection").returns({
+      find: function() {
+        return {
+          toArray: function(cb) {
+            cb(null, dummyStudentResults); 
+          }
+        };  // should return the same json that find usually returns 
+      }
+    });
+
+    db.collection("students").find().toArray(function(err, res){
+       console.log("in stub results: ", res); 
+    }); 
+    
+    done(); 
+  });
+  
 });
+
+// db.collection(‘students’).find({});  //returns JSON
+
 // 1. it should make the appropriate request
 // to the server on page load
 // 2 GET request to /students endpoint should 
