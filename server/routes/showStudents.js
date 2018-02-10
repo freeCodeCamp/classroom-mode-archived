f96cdb1815311e2b91b6b7f8f0726e6617c8b70a
Honorio Vega <hvega@csumb.edu>
2018-02-10 05:26:14 +0000

    Get /students return students' data

--- server/routes/showStudents.js
+++ server/routes/showStudents.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var db = require('./helpers/database_singleton').getDbInstance(); 
+var db = require('./helpers/database_singleton').getDbInstance();
 
 
 router.get('/', function(req, res) {
-  res.sendStatus(200);
+  db.collection("students").find({}).toArray(function(err, result) {
+    res.status(200).json(result);
+  })
 });
 
 module.exports = router;
