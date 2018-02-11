8baeba45def2b26f7abf83dc187a6dc8028e6d2d
cullenm <cmarchenko@csumb.edu>
2018-02-11 04:59:57 +0000

    Add tests for case where DB is empty

--- server/test/showStudentTest.js
+++ server/test/showStudentTest.js
@@ -4,27 +4,35 @@ var app = require('../app');
 var request = require('supertest');
 var db = require('../routes/helpers/database_singleton').getDbInstance();
 
+var sandbox = sinon.sandbox.create();
+
+afterEach(function(){
+   sandbox.restore(); 
+});
 
 describe('GET /students', () => {
-
-  var dummyStudentResults = [{ _id: "5a28cd1b1805592081cd31ea",
-  name: 'studentName',
-  username: 'studentUserName',
-  email: 'studentEmail',
-  notes: 'studentNote',
-  __v: 0 }];
-
-  sinon.stub(db, "collection").returns({
-    find: function() {
-      return {
-        toArray: function(cb) {
-          cb(null, dummyStudentResults);
-        }
-      };
-    }
-  });
+  
+  function stubDB(dummyStudentResults) {
+    sandbox.stub(db, "collection").returns({
+      find: function() {
+        return {
+          toArray: function(cb) {
+            cb(null, dummyStudentResults);
+          }
+        };
+      }
+    });
+  }
+  
 
   it('should return 200', (done) => {
+    var dummyStudentResults = [{ _id: "5a28cd1b1805592081cd31ea",
+      name: 'studentName',
+      username: 'studentUserName',
+      email: 'studentEmail',
+      notes: 'studentNote',
+      __v: 0 }];
+    stubDB(dummyStudentResults);
     request(app)
       .get('/students')
       .end(function(_err, res) {
@@ -34,11 +42,28 @@ describe('GET /students', () => {
   });
 
   it('should fetch data from mongo DB', (done) => {
+    var dummyStudentResults = [{ _id: "5a28cd1b1805592081cd31ea",
+      name: 'studentName',
+      username: 'studentUserName',
+      email: 'studentEmail',
+      notes: 'studentNote',
+      __v: 0 }];
+    stubDB(dummyStudentResults);
     request(app).get("/students").end(function(_err, res) {
       expect(JSON.parse(res.text)[0].name).to.equal(dummyStudentResults[0].name);
       expect(JSON.parse(res.text)[0].username).to.equal(dummyStudentResults[0].username);
       expect(JSON.parse(res.text)[0].email).to.equal(dummyStudentResults[0].email);
       expect(JSON.parse(res.text)[0].notes).to.equal(dummyStudentResults[0].notes);
+      done();
+    })
+  });
+
+  it('should return a 200 and an empty array if the database is empty', (done) => {
+    var dummyStudentResults = [];
+    stubDB(dummyStudentResults);
+    request(app).get("/students").end(function(_err, res) {
+      expect(res.status).to.equal(200);
+      expect(JSON.parse(res.text)).to.be.an('array').that.is.empty;
       done();
     })
   });
