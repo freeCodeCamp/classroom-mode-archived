930ae15ea78bc3a6ca66be0fb3e62f3c13fe1c40
Jason Kirn <jkirn@csumb.edu>
2018-02-12 05:06:05 +0000

    ClassTable invoke fetch on load

--- client/src/ClassTable.js
+++ client/src/ClassTable.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class ClassTable extends Component {
+  componentDidMount() {
+    fetch();
+  }
+
   render() {
     return (
       <div className="ClassTable">
--- client/src/__test__/ClassTable.test.js
+++ client/src/__test__/ClassTable.test.js
@@ -3,8 +3,13 @@ import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 import ClassTable from './../ClassTable';
+import mockResponse from './mock/response';
 
 describe("ClassTable", () => {
+  window.fetch = jest.fn().mockImplementation(function() {
+    return Promise.resolve(mockResponse(200, null, '{}'));
+  });
+
   let props;
   let mountedClassTable;
   const classTable = () => {
@@ -20,4 +25,9 @@ describe("ClassTable", () => {
     const divs = classTable().find("div");
     expect(divs.length).toBeGreaterThan(0);
   });
+
+  it("fetches GET /students", () => {
+    const fetchSpy = jest.spyOn(window, 'fetch');
+    expect(fetchSpy).toHaveBeenCalled();
+  })
 })
