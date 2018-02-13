7141c00837c8fcc47a0b829a094474979c7f1a34
Jason Rivera <jrivera2850@gmail.com>
2018-02-13 05:24:33 +0000

    displays classroom is empty when student list is empty

--- client/src/ClassTable.js
+++ client/src/ClassTable.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class ClassTable extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      errors: []
+    }
+  }
+
   componentDidMount() {
-    fetch('/students');
+    fetch('/students')
+      .then(function(res) {
+        res.json().then(function(data){
+          if(data.length === 0){
+            this.setState({ errors: ["classroom is empty"] });
+          }
+        }.bind(this));
+      }.bind(this));
   }
 
   render() {
