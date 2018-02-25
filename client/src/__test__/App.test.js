2f3f016fa59d0ea890e145264379c232aa2ecfdf
Jason Kirn <jkirn@csumb.edu>
2018-02-25 04:49:24 +0000

    Fixed App.js not render properly
    fetchStudentList was not in the right scope
    add missing students and error state
    add App.test.js to prevent future rendering problems

---
+++ client/src/__test__/App.test.js
@@ -0,0 +1,34 @@
+import Enzyme, { mount } from 'enzyme';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+import App from './../App';
+import mockResponse from './mock/response';
+
+describe("App", () => {
+  let mountedApp;
+  const app = () => {
+    if (!mountedApp) {
+      mountedApp = mount(
+        <App/>
+      );
+    }
+    return mountedApp;
+  };
+
+  beforeAll(() =>{
+    window.fetch = jest.fn().mockImplementation(function() {
+      return Promise.resolve(mockResponse(200, null, JSON.stringify([])));
+    });
+  });
+
+  afterAll(() =>{
+    window.fetch.mockReset();
+  });
+
+  it("always renders a div", () => {
+    const divs = app().find("div");
+    expect(divs.length).toBeGreaterThan(0);
+  });
+
+});
