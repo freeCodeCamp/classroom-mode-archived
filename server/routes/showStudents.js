703ded6daed0094a62fe24d4f4afcc45ad5815c7
Tom Lee <user512@users.noreply.github.com>
2018-03-04 04:49:34 +0000

    Inject daysInactive value to students object
    * Stub scraper function to avoid hitting scraper directly
    * We discovered some test in showStudentTest.js are hitting the scraper

--- server/routes/showStudents.js
+++ server/routes/showStudents.js
@@ -7,12 +7,12 @@ var scraper = require('./helpers/scraper');
 router.get('/', function(req, res) {
   db.collection("students").find({}).toArray(function(err, result) {
     result.forEach(function(item, index){
-      scraper.fetchUserInfoFromFCC(item.username, function(){
-
+      scraper.fetchUserInfoFromFCC(item.username, function(_err, fccResults){
+        item.daysInactive = fccResults.daysInactive;
       });
-    })
+    });
     res.status(200).json(result);
-  })
+  });
 });
 
 module.exports = router;
