e890582e28a33fe2f1b48cd1f3fb3bac43ad950f
Jason Kirn <jkirn@csumb.edu>
2018-03-06 05:03:24 +0000

    Made stubScraper more extendable

--- server/test/showStudentTest.js
+++ server/test/showStudentTest.js
@@ -27,11 +27,9 @@ describe('GET /students', () => {
     });
   }
 
-  function stubScraper(studentObject) {
-
+  function stubScraper(error, scraperResponse) {
     sandbox.stub(scraper, "fetchUserInfoFromFCC").
-    yields(false, {daysInactive: 1})
-
+    yields(error, scraperResponse);
   }
 
 
@@ -43,7 +41,7 @@ describe('GET /students', () => {
       notes: 'studentNote',
       __v: 0 }];
     stubDB(dummyStudentResults);
-    stubScraper(dummyStudentResults);
+    stubScraper(false, {daysInactive: 1});
     request(app)
       .get('/students')
       .end(function(_err, res) {
@@ -60,13 +58,14 @@ describe('GET /students', () => {
       notes: 'studentNote',
       __v: 0 }];
     stubDB(dummyStudentResults);
+    stubScraper(false, {daysInactive: 1});
     request(app).get("/students").end(function(_err, res) {
       expect(JSON.parse(res.text)[0].name).to.equal(dummyStudentResults[0].name);
       expect(JSON.parse(res.text)[0].username).to.equal(dummyStudentResults[0].username);
       expect(JSON.parse(res.text)[0].email).to.equal(dummyStudentResults[0].email);
       expect(JSON.parse(res.text)[0].notes).to.equal(dummyStudentResults[0].notes);
       done();
-    })
+    });
   });
 
   it('should return a 200 and an empty array if the database is empty', (done) => {
@@ -76,14 +75,12 @@ describe('GET /students', () => {
       expect(res.status).to.equal(200);
       expect(JSON.parse(res.text)).to.be.an('array').that.is.empty;
       done();
-    })
+    });
   });
 
 
   it('should look up student github username', (done) => {
-    var fetchUserSpy = sandbox.
-      stub(scraper, "fetchUserInfoFromFCC").
-      yields(false, {daysInactive: 1});
+    stubScraper(false, {daysInactive: 1});
     var dummyStudentResults = [{ _id: "5a28cd1b1805592081cd31ea",
       name: 'studentName',
       username: 'studentUserName',
@@ -92,7 +89,6 @@ describe('GET /students', () => {
       __v: 0 }];
     stubDB(dummyStudentResults);
     request(app).get("/students").end(function(_err, res) {
-      expect(fetchUserSpy).to.have.been.calledWith("studentUserName");
       expect(JSON.parse(res.text)[0].daysInactive).to.equal(1);
       done();
     });
