6d5970fc05f7b3f10809d8d0973ed357f8f28a60
Tom Lee <user512@users.noreply.github.com>
2018-03-09 05:02:23 +0000

    Ensure daysInactive data present before render response
    * Server test is still failing

--- server/routes/showStudents.js
+++ server/routes/showStudents.js
@@ -3,15 +3,23 @@ var router = express.Router();
 var db = require('./helpers/database_singleton').getDbInstance();
 var scraper = require('./helpers/scraper');
 
-
 router.get('/', function(req, res) {
+  var numStudents = 0;
+  var numResponsesReceived = 0;
   db.collection("students").find({}).toArray(function(err, result) {
+    numStudents = result.length;
+    if (numStudents === 0) {
+      res.status(200).json(result);
+    }
     result.forEach(function(item, index){
       scraper.fetchUserInfoFromFCC(item.username, function(_err, fccResults){
         item.daysInactive = fccResults.daysInactive;
+        numResponsesReceived++;
+        if (numResponsesReceived >= numStudents) {
+          res.status(200).json(result);
+        }
       });
     });
-    res.status(200).json(result);
   });
 });
 
