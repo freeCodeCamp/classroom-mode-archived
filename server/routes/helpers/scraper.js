22a15bd75eb6546938c2b2c2ac5d55d9714147c3
Tom Lee <user512@users.noreply.github.com>
2018-03-15 04:11:13 +0000

    Daysinactive now respect challenge updated_at timestamp

--- server/routes/helpers/scraper.js
+++ server/routes/helpers/scraper.js
@@ -1,6 +1,6 @@
 // This is a module which helps us interact with the scraper
 // It exposes one public function, 'fetchUserInfoFromFCC' that takes a github username and a callback function
-// 'fetchUserInfoFromFCC' will call the callback function and pass in a success/failure flag into the callback.  
+// 'fetchUserInfoFromFCC' will call the callback function and pass in a success/failure flag into the callback.
 //     if successful, the results of the api call should also be included as second parameter into the callback
 var request = require('request');
 
@@ -9,50 +9,55 @@ const fccBaseUrl = 'https://fcc-profile-scraper.herokuapp.com/user/';
 
 function fetchUserInfoFromFCC(githubName, callback) {
     request.get(fccBaseUrl + githubName, function (error, response, body){
-          console.log("Received scraper response"); 
-          
+          console.log("Received scraper response");
+
           if (error) {
-               console.log("Error: " + error); 
-               callback(true, {error: error}); 
-               return; 
+               console.log("Error: " + error);
+               callback(true, {error: error});
+               return;
           }
-          
+
           if (response.statusCode != 200) {
-               console.log("Error: status code: " + response.statusCode); 
-               callback(true, {error: JSON.parse(body)}); 
-               return; 
+               console.log("Error: status code: " + response.statusCode);
+               callback(true, {error: JSON.parse(body)});
+               return;
           }
-    
+
           var fccResults = JSON.parse(body);
           fccResults.daysInactive = computeDaysInactive(fccResults.completedChallenges);
-          callback(false, fccResults); 
-     }); 
+          callback(false, fccResults);
+     });
 }
 
 function findMostRecentCompletedChallenge(completedChallenges) {
-    var mostRecentDate = new Date ("Jan 1, 1980"); 
-    
+    var mostRecentDate = new Date ("Jan 1, 1980");
+
     for (var challenge of completedChallenges) {
-        var completedDate = new Date(challenge.completed_at); 
-        if (completedDate > mostRecentDate) {
-            mostRecentDate = completedDate;
+        var completedDate = new Date(challenge.completed_at);
+        var updatedDate = new Date(challenge.updated_at);
+        var lastActiveDate = completedDate;
+        if (updatedDate > completedDate) {
+          lastActiveDate = updatedDate;
+        }
+        if (lastActiveDate > mostRecentDate) {
+            mostRecentDate = lastActiveDate;
         }
     }
-    
-    return mostRecentDate; 
+
+    return mostRecentDate;
 }
 
 function computeDaysInactive(completedChallenges) {
     if (completedChallenges.length == 0) {
-        return "N/A"; 
+        return "N/A";
     }
-    
-    var mostRecentDate = findMostRecentCompletedChallenge(completedChallenges); 
-    var currentDate = new Date();   
+
+    var mostRecentDate = findMostRecentCompletedChallenge(completedChallenges);
+    var currentDate = new Date();
     var daysInactive = (currentDate - mostRecentDate) / (1000 * 60 * 60 * 24);
-    console.log('days inactive: ', daysInactive); 
-    daysInactive = Math.floor(daysInactive); 
-    return daysInactive; 
+    console.log('days inactive: ', daysInactive);
+    daysInactive = Math.floor(daysInactive);
+    return daysInactive;
 }
 
 exports.fetchUserInfoFromFCC = fetchUserInfoFromFCC;
--- server/test/scraper.js
+++ server/test/scraper.js
@@ -1,6 +1,6 @@
-var should = require('chai').should(); 
-var expect = require('chai').expect; 
-var scraper = require('../routes/helpers/scraper'); 
+var should = require('chai').should();
+var expect = require('chai').expect;
+var scraper = require('../routes/helpers/scraper');
 var sinon = require('sinon');
 var request = require('request');
 
@@ -8,19 +8,19 @@ var request = require('request');
 var sandbox = sinon.sandbox.create();
 
 afterEach(function(){
-   sandbox.restore(); 
-}); 
-   
-   
+   sandbox.restore();
+});
+
+
 it('should return an error if scraper has a non-200 status code', function(done) {
    var get = sandbox.stub(request, "get");
-   
+
    get.yieldsOn(this, null, {statusCode:400}, "{}");
-   
+
    scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-       expect(err).to.equal(true); 
-       expect(results.error).to.be.a('object'); 
-       done(); 
+       expect(err).to.equal(true);
+       expect(results.error).to.be.a('object');
+       done();
    })
 });
 
@@ -28,7 +28,7 @@ it('should return an error if scraper has a non-200 status code', function(done)
 
 it('should return no errors if scraper has a 200 status code', function(done) {
   var get = sandbox.stub(request, "get");
-   
+
    var testData = JSON.stringify({
       "name": "Utsab Saha",
       "profileImage": "https://avatars3.githubusercontent.com/u/6780322?v=4",
@@ -54,20 +54,20 @@ it('should return no errors if scraper has a 200 status code', function(done) {
          }
          ]
       });
-   
+
    get.yieldsOn(this, null, {statusCode:200}, testData);
-   
+
    scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-       expect(err).to.equal(false); 
-       done(); 
+       expect(err).to.equal(false);
+       done();
    })
 });
 
 it('should compute correct number of inactive days', function(done) {
       var get = sandbox.stub(request, "get");
-      var now = new Date("December 17, 2017 11:13:00");
+      var now = new Date("December 17, 2017");
       var clock = sandbox.useFakeTimers(now.getTime());
-   
+
    var testData = JSON.stringify({
       "name": "Utsab Saha",
       "profileImage": "https://avatars3.githubusercontent.com/u/6780322?v=4",
@@ -87,18 +87,18 @@ it('should compute correct number of inactive days', function(done) {
          },
          {
             "title": "Configure your Code Portfolio",
-            "completed_at": "Dec 15, 2017",  
+            "completed_at": "Dec 15, 2017",
             "updated_at": "",
             "url": "https://www.freecodecamp.com/challenges/Configure your Code Portfolio"
          }
          ]
       });
-   
+
    get.yieldsOn(this, null, {statusCode:200}, testData);
-   
+
    scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-       expect(results.daysInactive).to.equal(2); 
-       done(); 
+       expect(results.daysInactive).to.equal(2);
+       done();
    })
 });
 
@@ -106,19 +106,19 @@ it('should compute correct number of inactive days', function(done) {
 
 it('should compute correct number of inactive days when user has no history', function(done) {
    var get = sandbox.stub(request, "get");
-   
+
    var testData = JSON.stringify({
       "name": "Utsab Saha",
       "profileImage": "https://avatars3.githubusercontent.com/u/6780322?v=4",
       "location": "San Francisco",
       "completedChallenges": []
       });
-   
+
    get.yieldsOn(this, null, {statusCode:200}, testData);
-   
+
    scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-       expect(results.daysInactive).to.equal("N/A"); 
-       done(); 
+       expect(results.daysInactive).to.equal("N/A");
+       done();
    })
 });
 
@@ -126,9 +126,9 @@ it('should compute correct number of inactive days when user has no history', fu
 
 it('should compute correct number of inactive days when user was active today', function(done) {
    var get = sandbox.stub(request, "get");
-   var now = new Date("December 17, 2017 11:13:00");
+   var now = new Date("December 17, 2017");
    var clock = sandbox.useFakeTimers(now.getTime());
-   
+
    var testData = JSON.stringify({
       "name": "Utsab Saha",
       "profileImage": "https://avatars3.githubusercontent.com/u/6780322?v=4",
@@ -137,7 +137,7 @@ it('should compute correct number of inactive days when user was active today', 
          {
             "title": "Learn how Free Code Camp Works",
             "completed_at": "Jul 05, 2016",
-            "updated_at": "",
+            "updated_at": "Dec 16, 2017",
             "url": "https://www.freecodecamp.com/challenges/Learn how Free Code Camp Works"
          },
          {
@@ -148,17 +148,56 @@ it('should compute correct number of inactive days when user was active today', 
          },
          {
             "title": "Configure your Code Portfolio",
-            "completed_at": "Dec 17, 2017",  
+            "completed_at": "Dec 17, 2017",
             "updated_at": "",
             "url": "https://www.freecodecamp.com/challenges/Configure your Code Portfolio"
          }
          ]
       });
-   
+
    get.yieldsOn(this, null, {statusCode:200}, testData);
-   
+
    scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-       expect(results.daysInactive).to.equal(0); 
-       done(); 
+       expect(results.daysInactive).to.equal(0);
+       done();
+   })
+});
+
+it('should respect last updated_at when calculating last daysInactive', function(done) {
+      var get = sandbox.stub(request, "get");
+      var now = new Date("December 17, 2017");
+      var clock = sandbox.useFakeTimers(now.getTime());
+
+   var testData = JSON.stringify({
+      "name": "Utsab Saha",
+      "profileImage": "https://avatars3.githubusercontent.com/u/6780322?v=4",
+      "location": "San Francisco",
+      "completedChallenges": [
+         {
+            "title": "Learn how Free Code Camp Works",
+            "completed_at": "Jul 05, 2016",
+            "updated_at": "Dec 16, 2017",
+            "url": "https://www.freecodecamp.com/challenges/Learn how Free Code Camp Works"
+         },
+         {
+            "title": "Create a GitHub Account and Join our Chat Rooms",
+            "completed_at": "Jul 06, 2016",
+            "updated_at": "",
+            "url": "https://www.freecodecamp.com/challenges/Create a GitHub Account and Join our Chat Rooms"
+         },
+         {
+            "title": "Configure your Code Portfolio",
+            "completed_at": "Dec 15, 2017",
+            "updated_at": "",
+            "url": "https://www.freecodecamp.com/challenges/Configure your Code Portfolio"
+         }
+         ]
+      });
+
+   get.yieldsOn(this, null, {statusCode:200}, testData);
+
+   scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
+       expect(results.daysInactive).to.equal(1);
+       done();
    })
 });
