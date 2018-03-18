67677768572c1d4dbbcc849f24772df2e0eac866
Tom Lee <user512@users.noreply.github.com>
2018-03-18 02:05:09 +0000

    Server returns completed challenges data

--- server/test/showStudentTest.js
+++ server/test/showStudentTest.js
@@ -89,4 +89,28 @@ describe('GET /students', () => {
       done();
     });
   });
+
+  it('should returns completedChallenges count and titles', (done) => {
+    stubScraper(
+      false,
+      {
+        completedChallenges: [
+          { title: 'Reverse a String' },
+          { title: 'Say Hello to HTML Elements' }
+          ]
+      });
+    var dummyStudentResults = [{ _id: "5a28cd1b1805592081cd31ea",
+      name: 'studentName',
+      username: 'studentUserName',
+      email: 'studentEmail',
+      notes: 'studentNote',
+      __v: 0 }];
+    stubDB(dummyStudentResults);
+    request(app).get("/students").end(function(_err, res) {
+      expect(JSON.parse(res.text)[0].completedChallengesCount).to.equal(2);
+      expect(JSON.parse(res.text)[0].completedChallenges[0].title).to.equal('Reverse a String');
+      expect(JSON.parse(res.text)[0].completedChallenges[1].title).to.equal('Say Hello to HTML Elements');
+      done();
+    });
+  });
 });
