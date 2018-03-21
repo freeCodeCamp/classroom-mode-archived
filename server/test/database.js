8d76d8b5f5b74c68e426c762e8295e1eab582ef3
Utsab <utsab.k.saha@gmail.com>
2018-03-21 04:20:44 +0000

    Close data base connection after tests

---
+++ server/test/database.js
@@ -0,0 +1,18 @@
+/**
+ * Our backend tests currently behave more like integration tests than unit
+ * tests, because many of them spin up the server (including the database
+ * connection) as setup for the tests.  There was a problem where the
+ * database connection did not close after the tests completed which
+ * would then cause the travis-CI tests to hang indefinitely.  Therefore,
+ * we created this file to close the database connection after all
+ * the tests are finished running.
+ */
+var databaseModule = require('../routes/helpers/database_singleton');
+
+after(function(){
+  databaseModule.closeConnection();
+  console.log("Closing DB connection #############################");
+});
+
+
+
