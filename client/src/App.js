6f8449e2b5a039b509a2de963322ad80e8d803bf
Honorio Vega <hvega@csumb.edu>
2018-04-10 03:23:06 +0000

    Added NavBar component and test

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import AddStudentForm from './AddStudentForm';
 import ClassTable from './ClassTable';
+import NavBar from './NavBar';
 
 
 function DisplayUserInfo(props) {
@@ -49,31 +50,14 @@ class App extends Component {
       });
   }
 
-  handleSearch(e) {
-    var githubName = document.getElementById('github-name-input').value;
-
-    if (!githubName) return;
-
-
-    fetch('/users/' + githubName)
-      .then(res => res.json())
-      .then((user_info) => {
-        this.setState({ user_info });
-      });
-  };
-
   render() {
     return (
       <div className="App">
-        <h1>User Info</h1>
-          <input type="text" id="github-name-input"></input>
-          <button onClick={this.handleSearch.bind(this)}>Search</button>
-          <DisplayUserInfo user_info={this.state.user_info}/>
-          <AddStudentForm fetchStudentsFromParent={this.fetchStudentList.bind(this)}/>
-          <ClassTable students={this.state.students} errors={this.state.errors}/>
+        <NavBar />
+        <AddStudentForm fetchStudentsFromParent={this.fetchStudentList.bind(this)}/>
+        <ClassTable students={this.state.students} errors={this.state.errors}/>
       </div>
     );
   }
 }
-
 export default App;
