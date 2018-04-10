95d86f696dd4453a6bb11551ef4b3f22ceb57232
Honorio Vega <hvega@csumb.edu>
2018-04-10 03:57:23 +0000

    Cleaned App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AddStudentForm from './AddStudentForm';
 import ClassTable from './ClassTable';
 import NavBar from './NavBar';
 
 
-function DisplayUserInfo(props) {
-     if (props.user_info.name) {
-       return (
-         <div>
-           <div> {props.user_info.name} </div>
-           <div> {props.user_info.completedChallenges.length} challenges completed</div>
-           <div> Days Inactive: {props.user_info.daysInactive} </div>
-         </div>
-       );
-     } else {
-        return (
-          <div>  </div>
-        );
-     }
-}
 
 class App extends Component {
 
