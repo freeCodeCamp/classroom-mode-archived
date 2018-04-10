e3b39222351e999e3bb8f09f30c46927893b863c
Honorio Vega <hvega@csumb.edu>
2018-04-10 03:44:56 +0000

    Added css to NavBar

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import './NavBar.css';
 
 class NavBar extends Component {
   render() {
     return(
-    <p className="NavBar">placeholder</p>
+      <div  className="NavBar">
+        <img alt="free-code-camp-logo"
+             src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg" />
+        <span id='navbar-title'>Teacher Dashboard <b>BETA</b> </span>
+      </div>
     );
   }
 }
