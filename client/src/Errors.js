0d8bb6ea0a0d04828a8c472640e10bab572b60ef
Joseph Chambers <joseph@michael-chambers.com>
2018-04-18 16:37:52 -0700

    Refactor

--- client/src/Errors.js
+++ client/src/Errors.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Errors extends Component {
   render() {
-    return(
-        <div className="errors">
-          {this.props.errors.map(function(error, i) {
-            return <div key={i}>{error}</div>;
-          })}
-        </div>
-      );
+    return (
+      <div className="errors">
+        {this.props.errors.map(function(error, i) {
+          return <div key={i}>{error}</div>;
+        })}
+      </div>
+    );
   }
 }
-
 
 export default Errors;
--- client/src/StudentRow.js
+++ client/src/StudentRow.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class StudentRow extends Component {
-  render() {
-    return(
-      <tr>
-        <td>{this.props.name}</td>
-        <td>{this.props.username}</td>
-        <td>{this.props.email}</td>
-        <td>{this.props.notes}</td>
-        <td>{this.props.daysInactive}</td>
-        <td>{this.props.newSubmissionsCount}</td>
-      </tr>
-    );
-  }
+const StudentRow = (props) => {
+  let { name, username, email, notes, daysInactive, newSubmissionsCount } = props;
+  
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{username}</td>
+      <td>{email}</td>
+      <td>{notes}</td>
+      <td>{daysInactive}</td>
+      <td>{newSubmissionsCount}</td>
+    </tr>
+  )
 }
 
 export default StudentRow;
