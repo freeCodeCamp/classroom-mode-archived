e1e4b841fe57581f02a0564cce5d9acc111f74c9
Joseph Chambers <joseph@michael-chambers.com>
2018-04-22 18:05:04 -0700

    removed duplicates (when it was nested)

--- server/test/scraper.js
+++ server/test/scraper.js
@@ -95,21 +95,6 @@ describe('it should scrap data from FCC', () => {
     }
   })
 
-  it('should compute correct number of inactive days when user has no history', done => {
-    try {
-      let get = sandbox.stub(request, 'get')
-
-      get.yieldsOn(this, null, {statusCode: 200}, userData)
-
-      scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-        expect(results.daysInactive).to.equal('N/A')
-        done()
-      })
-    } catch (e) {
-      console.log(`Error! should compute correct number of inactive days when user has no history: ${e}`)
-    }
-  })
-
   it('should compute correct number of inactive days when user was active today', done => {
     try {
       let get = sandbox.stub(request, 'get')
@@ -144,37 +129,6 @@ describe('it should scrap data from FCC', () => {
     }
   })
 
-
-  it('should return an error if scraper has a non-200 status code', function (done) {
-    try {
-      let get = sandbox.stub(request, 'get')
-      get.yieldsOn(this, null, {statusCode: 400}, '{}')
-
-      scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-          expect(err).to.equal(true)
-          expect(results.error).to.be.a('object')
-          done()
-      })
-    } catch (e) {
-      console.log(`Error! should return an error if scraper has a non-200 status code: ${e}`)
-    }
-  })
-
-  it('should return no errors if scraper has a 200 status code', done => {
-    try {
-      let get = sandbox.stub(request, 'get')
-
-      get.yieldsOn(this, null, {statusCode: 200}, testData)
-  
-      scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-        expect(err).to.equal(false)
-        done()
-      })
-    } catch (e) {
-      console.log(`Error! should return no errors if scraper has a 200 status code: ${e}`)
-    }
-  })
-
   it('should compute correct number of inactive days when user has no history', done => {
     try {
       let get = sandbox.stub(request, 'get')
@@ -190,51 +144,5 @@ describe('it should scrap data from FCC', () => {
     }
   })
 
-  it('should compute correct number of inactive days when user was active today', done => {
-    try {
-      let get = sandbox.stub(request, 'get')
-      let now = new Date('December 17, 2017')
-      let clock = sandbox.useFakeTimers(now.getTime())
-  
-      get.yieldsOn(this, null, {statusCode: 200}, testData)
-  
-      scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-        expect(results.daysInactive).to.equal(0)
-        done()
-      })
-    } catch (e) {
-      console.log(`Error! should compute correct number of inactive days when user was active today: ${e}`)
-    }
-  })
-
-  xit('should compute correct number of inactive days', done => {
-    try {
-      let get = sandbox.stub(request, 'get')
-      let now = new Date('December 17, 2017')
-      let clock = sandbox.useFakeTimers(now.getTime())
-  
-      get.yieldsOn(this, null, {statusCode: 200}, testData)
-  
-      scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-        expect(results.daysInactive).to.equal(2)
-        done()
-      })
-    } catch (e) {
-      console.log(`Error: ${e}`)
-    }
-  })
-
-  xit('should respect last updated_at when calculating last daysInactive', function(done) {
-    var get = sandbox.stub(request, 'get')
-    var now = new Date('December 17, 2017')
-    var clock = sandbox.useFakeTimers(now.getTime())
-
-    get.yieldsOn(this, null, {statusCode: 200}, testData)
-
-    scraper.fetchUserInfoFromFCC('testUser', function(err, results) {
-      expect(results.daysInactive).to.equal(1)
-      done()
-    })
-  })
 })
 
