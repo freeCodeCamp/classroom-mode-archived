0bad1ddca05d1c25662a8e50f4ae5d03542b5607
Joseph Chambers <joseph@michael-chambers.com>
2018-04-24 13:44:34 -0700

    Boilerplate for mobx with decorator support.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './containers/App'
-import registerServiceWorker from './registerServiceWorker'
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'mobx-react'
+import UiStore from './stores/UiStore'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+const Root = (
+  <Provider UiStore={UiStore}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(Root, document.getElementById('root'));
+registerServiceWorker();
