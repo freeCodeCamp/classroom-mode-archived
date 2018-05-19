5cfd784070f08b60e226408af43131d23817c783
Tom Lee <user512@users.noreply.github.com>
2018-05-19 14:34:00 -0700

    Remove client console.log and fix app title

--- client/src/components/main/App.js
+++ client/src/components/main/App.js
@@ -29,7 +29,6 @@ export default class App extends Component {
   handleDelete = studentId => {
     try {
       axios.delete(`/students/${studentId}`)
-      console.log(this.state)
       this.setState(prevState => ({
         students: prevState.students.filter(
           student => student._id !== studentId
