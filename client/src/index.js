48f9deebb1054333fe11cb2a5a8562479aba6b84
Joseph Chambers <joseph@michael-chambers.com>
2018-06-21 00:29:07 -0700

    adds apollo to the frontend.

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import { HttpLink } from 'apollo-link-http'
 import ErrorBoundary from './components/main/ErrorBoundary'
 import './index.css'
 import RouterSetup from './components/main/RouterSetup'
 import registerServiceWorker from './registerServiceWorker'
 
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: '/graphql',
+  }),
+  connectToDevTools: true,
+})
+
 class Root extends Component {
   render() {
     return (
-      <ErrorBoundary
-        render={() => <div className="error">Error has occurred</div>}
-      >
-        <RouterSetup />
-      </ErrorBoundary>
+      <ApolloProvider client={client}>
+        <ErrorBoundary
+          render={() => <div className="error">Error has occurred</div>}
+        >
+          <RouterSetup />
+        </ErrorBoundary>
+      </ApolloProvider>
     )
   }
 }
