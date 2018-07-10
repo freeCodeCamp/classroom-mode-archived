a872bfef6bb9595c543c60e5e78f8b4a092dab5b
Tom Lee <user512@users.noreply.github.com>
2018-07-09 20:32:14 -0700

    Remove userController, not using it

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,11 @@ const express = require('express')
 const router = express.Router()
 const indexController = require('../controllers/indexController')
 const studentController = require('../controllers/studentController')
-const userController = require('../controllers/userController')
 
 router.get('/', indexController.getHome)
 router.delete('/students/:studentId', studentController.deleteStudent)
 router.get('/students', studentController.showStudent)
 router.put('/students', studentController.updateStudent)
 router.post('/students', studentController.addStudent)
-router.get('/users/:userId', userController.getUserById)
 
 module.exports = router
